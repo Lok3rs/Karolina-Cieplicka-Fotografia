984e73c89aecce60c3198f55e60cd58db45bd47b
Dawid Adamski <lok3rsss@gmail.com>
2020-03-21 13:43:59 +0100

    add auth routes

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,51 @@ const express = require("express"),
 router.get("/", function(req, res) {
   res.render("landing");
 });
+// =======================
+// AUTH/LOGIN ROUTES
+// =======================
+router.get("/register", (req, res) => res.render("auth/register"));
+
+router.post("/register", (req, res) => {
+  User.register(
+    new User({
+      username: req.body.username,
+      email: req.body.email
+    }),
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        req.flash("error", err.message);
+        res.redirect("/register");
+      }
+      passport.authenticate("local")(req, res, () => {
+        req.flash(
+          "success",
+          `Succesfully registrated! Welcome ${user.username}!`
+        );
+        res.redirect("/blog");
+      });
+    }
+  );
+});
+router.get("/login", (req, res) => res.render("auth/login"));
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/blog",
+    failureRedirect: "/login",
+    failureFlash: true
+  }),
+  (req, res) => {
+    console.log(`${currentUser.username} logged in`);
+  }
+);
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success", "Logged out! See you again!");
+  res.redirect("/blog");
+});
 
 module.exports = router;
