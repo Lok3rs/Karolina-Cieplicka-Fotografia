2c9971a559866406cfbd02aafc052c6065bb50d6
Dawid Adamski <lok3rsss@gmail.com>
2020-03-21 13:39:20 +0100

    add navbar

--- routes/blog.js
+++ routes/blog.js
@@ -3,4 +3,17 @@ const express = require("express"),
   router = express.Router(),
   middleware = require("../middleware");
 
+//  INDEX ROUTE
+router.get("/", (req, res) => {
+  Blog.find({}, (err, blog) => {
+    if (err) {
+      console.log("ERROR : ${err.message}");
+    } else {
+      res.render("blog/index", {
+        blog: blog
+      });
+    }
+  });
+});
+
 module.exports = router;
