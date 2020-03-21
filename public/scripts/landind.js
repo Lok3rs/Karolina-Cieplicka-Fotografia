fc4d94e990ac4b6f7271ed3c42219bb42cede08d
Dawid Adamski <lok3rsss@gmail.com>
2020-03-21 21:47:36 +0100

    add loader

---
+++ public/scripts/landind.js
@@ -0,0 +1,4 @@
+$(window).on("load", function() {
+  $(".loader-container").fadeOut("slow");
+  $("#landing-header").show();
+});
--- routes/blog.js
+++ routes/blog.js
@@ -16,4 +16,35 @@ router.get("/", (req, res) => {
   });
 });
 
+// NEW POST ROUTES
+router.get("/new", middleware.isLoggedIn, (req, res) => {
+  res.render("blog/new");
+});
+
+router.post("/", middleware.isLoggedIn, (req, res) => {
+  req.body.blog.body = req.sanitize(req.body.blog.body);
+  const title = req.body.blog.title,
+    img = req.body.blog.img,
+    content = req.body.blog.content,
+    author = {
+      id: req.user._id,
+      username: req.user.username
+    };
+  const newPost = {
+    title: title,
+    img: img,
+    content: content,
+    author: author
+  };
+  Blog.create(newPost, (err, newP) => {
+    if (err) {
+      req.flash("error", err.message);
+      res.render("blog/new");
+    } else {
+      req.flash("success", "Dodałaś post!");
+      res.redirect("/blog");
+    }
+  });
+});
+
 module.exports = router;
