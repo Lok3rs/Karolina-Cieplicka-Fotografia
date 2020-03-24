41ce6c64cddbdc69963926b6e7c821f04dc96259
Dawid Adamski <lok3rsss@gmail.com>
2020-03-24 23:24:16 +0100

    little css refactor

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,11 @@
 $(".carousel").carousel({
   interval: 3500
 });
+
+$(window).on("scroll", () => {
+  const navbar = $(".navbar");
+  navbar.css("background-color", "rgba(227, 168, 125, 0.5)");
+  if (navbar.position().top == 0) {
+    navbar.css("background-color", "");
+  }
+});
